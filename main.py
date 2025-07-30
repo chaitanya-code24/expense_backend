68cbbd7f7de8d7f56128c40d96f37cc563afb549
Chaitanya <chaitanyalokhande2412work@gmail.com>
2025-07-30 20:34:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ class Expense(BaseModel):
 # CORS config
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ai-expense-tracker-mw744xzbs-chaitanya-lokhandes-projects.vercel.app"],
+    allow_origins=["https://ai-expense-tracker-l8reqtq8g-chaitanya-lokhandes-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
